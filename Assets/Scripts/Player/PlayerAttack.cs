1ccbbf12e3be6512331825687e7abd14cc57f1e8
ththrh <44047525+ththrh@users.noreply.github.com>
2021-08-04 23:47:18 +0900

    Utopia start

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public int WCode = 0;
+    public float Delay = 0.5f;
+    public GameObject bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerScript.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float moveSpeed;
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //View Direction
+        Vector2 mPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 oPos = transform.position;
+        float xDif = mPos.x - oPos.x;
+        float yDif = mPos.y - oPos.y;
+        float xDifAbs = Mathf.Abs(xDif);
+        float yDifAbs = Mathf.Abs(yDif);
+        if (xDif < 0 && xDifAbs >= yDifAbs)
+        {
+            anim.SetInteger("ViewDir", 2);
+            transform.localScale = new Vector3(-5, 5, 1);
+        }
+        else if(xDif >= 0 && xDifAbs >= yDifAbs)
+        {
+            anim.SetInteger("ViewDir", 2);
+            transform.localScale = new Vector3(5, 5, 1);
+        }
+        else if(yDif < 0 && xDifAbs < yDifAbs)
+        {
+            anim.SetInteger("ViewDir", 0);
+        }
+        else if(yDif >=0 && xDifAbs < yDifAbs)
+        {
+            anim.SetInteger("ViewDir", 1);
+        }
+        //Move
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+            anim.SetTrigger("Walk");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+            anim.SetTrigger("Walk");
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(Vector3.up * moveSpeed * Time.deltaTime);
+            anim.SetTrigger("Walk");
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(Vector3.down * moveSpeed * Time.deltaTime);
+            anim.SetTrigger("Walk");
+        }
+    }
+}
