95c85b202930b33ae787a4a48bfa4101e86f346a
minwoo00044 <minwoo0004@gmail.com>
2021-10-06 07:10:49 +0900

    anim-imsee
    존나 어색함

---
+++ Assets/maps/MOVE.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MOVE : MonoBehaviour
+{
+    public float Speed;
+
+    Rigidbody2D rigid;
+    float h;
+    float v;
+    Animator anim;
+    SpriteRenderer rend;
+
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        rend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+
+        bool hdown =  Input.GetButtonDown("Horizontal");
+        bool hUP = Input.GetButtonUp("Horizontal");
+        bool vdown =  Input.GetButtonDown("Vertical");
+        bool vUP = Input.GetButtonUp("Vertical");
+
+
+        anim.SetInteger("hAxisRaw",(int)h);
+        anim.SetInteger("vAxisRaw",(int)v);
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rend.flipX = true;
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rend.flipX = false;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        rigid.velocity = new Vector2(h, v) * Speed;
+    }
+}
