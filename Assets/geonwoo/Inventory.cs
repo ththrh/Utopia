9098c64b07a3b4c3eafc0ca1998746e2f71b2632
Rose <82176176+geonwoo0215@users.noreply.github.com>
2021-10-08 03:31:49 +0900

    geonwoo inventory 3

--- Assets/geonwoo/Inventory.cs
+++ Assets/geonwoo/Inventory.cs
@@ -4,21 +4,72 @@ using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
-    public GameObject inventoryPanel;
-    bool activelnventory = false;
 
-    private void Start()
+
+    public static Inventory instance;
+    private void Awake()
     {
-        inventoryPanel.SetActive(activelnventory);
+        if(instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
     }
 
 
-    private void Update()
+
+    public delegate void OnSlotCountChange(int val);
+    public OnSlotCountChange onSlotCountChange;
+
+    public List<Item> items = new List<Item>();
+    public delegate void OnChangeItem();
+    public OnChangeItem onChangeItem;
+
+
+
+    private int slotCnt;
+    public int SlotCnt
     {
-        if(Input.GetKeyDown(KeyCode.I))
+        get => slotCnt;
+        set
         {
-            activelnventory = !activelnventory;
-            inventoryPanel.SetActive(activelnventory);
+            slotCnt = value;
+            onSlotCountChange.Invoke(slotCnt);
         }
     }
+
+    void Start()
+    {
+        SlotCnt = 4;
+    }
+
+
+    public bool AddItem(Item _item)
+    {
+        if (items.Count<SlotCnt)
+        {
+            items.Add(_item);
+            if(onChangeItem!=null)
+            onChangeItem.Invoke();
+            return true;
+        }
+        return false;
+    }
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("FieldItem"))
+        {
+            FieldItems fieldItems = collision.GetComponent<FieldItems>();
+            if (AddItem(fieldItems.GetItem()))
+                fieldItems.DestroyItem();
+
+        }
+    }
+
+
 }
