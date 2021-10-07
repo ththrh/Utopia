530253776cbc96313a624042fc8503e14f2f204b
Rose <82176176+geonwoo0215@users.noreply.github.com>
2021-10-07 13:42:56 +0900

    geonwoo inventory

---
+++ Assets/geonwoo/Inventory.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public GameObject inventoryPanel;
+    bool activelnventory = false;
+
+    private void Start()
+    {
+        inventoryPanel.SetActive(activelnventory);
+    }
+
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            activelnventory = !activelnventory;
+            inventoryPanel.SetActive(activelnventory);
+        }
+    }
+}
