bbf4a8e39835e22d3b22875b1f16d26fcd35c738
Rose <82176176+geonwoo0215@users.noreply.github.com>
2021-11-26 16:27:44 +0900

    geonwoo shoot circle

--- Assets/geonwoo/Bullets.cs
+++ Assets/geonwoo/Bullets.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class Bullets : MonoBehaviour
 {
-    GameObject target;
-    public float speed;
-    Rigidbody2D bulletRB;
+    //GameObject target;
+    //public float speed;
+    //Rigidbody2D bulletRB;
 
-    void Start()
+    //void Start()
+    //{
+    //    bulletRB = GetComponent<Rigidbody2D>();
+
+    //    target = GameObject.FindGameObjectWithTag("Player");
+    //    Vector3 Dir = new Vector3(1, 1,1);
+    //    Vector2 moveDir = (target.transform.position - transform.position + Dir).normalized* speed;
+
+    //    bulletRB.velocity = new Vector2(moveDir.x, moveDir.y);
+
+    //    Destroy(this.gameObject, 2);
+
+    //}
+    public float speed = 10f;
+
+    private void Start()
     {
-        bulletRB = GetComponent<Rigidbody2D>();
-        target = GameObject.FindGameObjectWithTag("Player");
-        Vector2 moveDir = (target.transform.position - transform.position).normalized * speed;
-        bulletRB.velocity = new Vector2(moveDir.x, moveDir.y);
-        Destroy(this.gameObject, 2);
-        
+        //생성으로부터 2초 후 삭제
+        Destroy(gameObject, 2f);
     }
 
-    
+    private void Update()
+    {
+        //두번째 파라미터에 Space.World를 해줌으로써 Rotation에 의한 방향 오류를 수정함
+        transform.Translate(Vector2.right * speed * Time.deltaTime, Space.Self);
+    }
+
+
 }
--- Assets/geonwoo/EnemyFollowShootPlayer.cs
+++ Assets/geonwoo/EnemyFollowShootPlayer.cs
@@ -12,7 +12,10 @@ public class EnemyFollowShootPlayer : MonoBehaviour
     public GameObject bullet;
     public GameObject bulletParent;
     private Transform player;
-
+    public int patternIndex;
+    public float rot_Speed;
+    public Transform pos;
+    public Transform target;
     // Start is called before the first frame update
     void Start()
     {
@@ -29,10 +32,75 @@ public class EnemyFollowShootPlayer : MonoBehaviour
         }
         else if (distanceFromPlayer <= shootingRange && nextFireTime < Time.time)
         {
-            Instantiate(bullet, bulletParent.transform.position, Quaternion.identity);
+            //원형 발사 후 추적
+            var bl = new List<Transform>();
+
+            for (int i = 0; i < 360; i += 13)
+            {
+                //총알 생성
+                var temp = Instantiate(bullet);
+
+                //2초후 삭제
+                Destroy(temp, 2f);
+
+                //총알 생성 위치를 (0,0) 좌표로 한다.
+                temp.transform.position = bulletParent.transform.position;
+
+                //?초후에 Target에게 날아갈 오브젝트 수록
+                bl.Add(temp.transform);
+
+                //Z에 값이 변해야 회전이 이루어지므로, Z에 i를 대입한다.
+                temp.transform.rotation = Quaternion.Euler(0, 0, i);
+            }
+            //총알을 Target 방향으로 이동시킨다.
+            StartCoroutine(BulletToTarget(bl));
+
+
+            //원형 발사
+            //for (int i = 0; i < 360; i += 13)
+            //{
+            //    //총알 생성
+            //    GameObject temp = Instantiate(bullet);
+
+            //    //2초마다 삭제
+            //    Destroy(temp, 2f);
+
+            //    //총알 생성 위치를 (0,0) 좌표로 한다.
+            //    temp.transform.position = bulletParent.transform.position;
+
+            //    //Z에 값이 변해야 회전이 이루어지므로, Z에 i를 대입한다.
+            //    temp.transform.rotation = Quaternion.Euler(0, 0, i);
+            //}
+
+
+
+            //그냥 발사
+            //Instantiate(bullet, bulletParent.transform.position, Quaternion.identity);
+            //Instantiate(bullet, bulletParent.transform.position, Quaternion.identity);
             nextFireTime = Time.time + fireRate;
         }
     }
+    IEnumerator BulletToTarget(List<Transform> bl)
+    {
+        //0.5초 후에 시작
+        yield return new WaitForSeconds(0.5f);
+
+        for (int i = 0; i < bl.Count; i++)
+        {
+            //현재 총알의 위치에서 플레이의 위치의 벡터값을 뻴셈하여 방향을 구함
+            var target_dir = target.transform.position - bl[i].position;
+
+            //x,y의 값을 조합하여 Z방향 값으로 변형함. -> ~도 단위로 변형
+            var angle = Mathf.Atan2(target_dir.y, target_dir.x) * Mathf.Rad2Deg;
+
+            //Target 방향으로 이동
+            bl[i].rotation = Quaternion.Euler(0, 0, angle);
+        }
+
+        //데이터 해제
+        bl.Clear();
+    }
+
 
     private void OnDrawGizmosSelected()
     {
