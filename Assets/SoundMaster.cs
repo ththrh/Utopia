bde6229995c5b22bbae4df2a64b0c01ce50ade21
ththrh <44047525+ththrh@users.noreply.github.com>
2022-02-15 15:01:38 +0900

    Sound Slider
    good

---
+++ Assets/SoundMaster.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SoundMaster : MonoBehaviour
+{
+    public AudioMixer masterMixer;
+    public Slider audioSlider;
+    public float sound;
+
+    public void AudioControl()
+    {
+        sound = audioSlider.value;
+        if (sound == -40f) masterMixer.SetFloat("BGM", -80);
+        else masterMixer.SetFloat("BGM", sound);
+    }
+}
--- Assets/utopia/GameManager.cs
+++ Assets/utopia/GameManager.cs
@@ -210,6 +210,10 @@ public class GameManager : MonoBehaviour
     }
     public void GameExit()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
