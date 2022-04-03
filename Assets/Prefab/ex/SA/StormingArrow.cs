3929cad34e8b0cab2dcc2865cb799683d8331829
ththrh <44047525+ththrh@users.noreply.github.com>
2022-04-04 03:49:31 +0900

    skill last

---
+++ Assets/Prefab/ex/SA/StormingArrow.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StormingArrow : MonoBehaviour
+{
+    [SerializeField] [Header("이동거리")] [Range(1f,9f)] float dist = 7f;
+    [SerializeField] [Header("이동속도")] [Range(1f, 50f)] float speed = 5f;
+    [SerializeField] [Header("파동빈도")] [Range(1f, 40f)] float frequency = 5f;
+    [SerializeField] [Header("파동높이")] [Range(0.2f, 4f)] float waveHeight = 5f;
+
+    Vector3 pos, localScale;
+    bool dirright = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pos = transform.position;
+        localScale = transform.localScale;
+        Destroy(gameObject, 3f);
+    }
+    private void Update()
+    {
+        go();
+    }
+    void go()
+    {
+        pos += transform.right * Time.deltaTime * speed;
+        transform.position = pos;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy"))
+        {
+            Debug.Log(1);
+            Destroy(gameObject);
+        }
+    }
+}
--- Assets/utopia/Player/bullet/PlayerBullet.cs
+++ Assets/utopia/Player/bullet/PlayerBullet.cs
@@ -9,9 +9,12 @@ public class PlayerBullet : MonoBehaviour
     public GameObject bullet;
     public GameObject bomb;
     public GameObject UCB;
+    public GameObject SA;
     public float ShotSpeed = 3.0f;
     public float shotTime;
     Status stat;
+    bool isAttack = false;
+
     void Start()
     {
         stat = GetComponentInParent<Status>();
@@ -39,8 +42,28 @@ public class PlayerBullet : MonoBehaviour
             else if (stat.click_isactiveskill_2)
             {
                 stat.Mp -= 1;
-                Instantiate(bomb, new Vector3(transform.position.x, transform.position.y-1, transform.position.z), Quaternion.AngleAxis(0, Vector3.forward));
+                Instantiate(bomb, new Vector3(transform.position.x, transform.position.y - 1, transform.position.z), Quaternion.AngleAxis(0, Vector3.forward));
                 GetComponent<AudioSource>().Play();
+            }
+            else if (stat.click_isactiveskill_3)
+            {
+                stat.Mp -= 1;
+                Instantiate(UCB, new Vector3(transform.position.x, transform.position.y - 1, transform.position.z), Quaternion.AngleAxis(0, Vector3.forward));
+                GetComponent<AudioSource>().Play();
+            }
+        }
+        if (stat.click_isactiveskill_4)
+        {
+            if (Input.GetMouseButton(1) && stat.Mp>0)
+            {
+                if (!isAttack)
+                {
+                    isAttack = true;
+                    stat.Mp -= 1;
+                    Instantiate(SA, transform.position, Quaternion.AngleAxis(angle, Vector3.forward));
+                    StartCoroutine("timecheck");
+                    GetComponent<AudioSource>().Play();
+                }
             }
         }
 
@@ -52,5 +75,11 @@ public class PlayerBullet : MonoBehaviour
                 GetComponent<AudioSource>().Play();
             }
         }
+    }
+
+    IEnumerator timecheck()
+    {
+        yield return new WaitForSeconds(0.25f);
+        isAttack = false;
     }
 }
