584e1ea2fd4c377a5361f1554266b4b3e7c1027b
leejiwon98 <84241553+leejiwon98@users.noreply.github.com>
2022-06-05 17:22:19 +0900

    teleport
    jiwon

---
+++ Assets/jiwon/Teleport.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject portal;
+    private GameObject player;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            player.transform.position = new Vector2(portal.transform.position.x, portal.transform.position.y);
+        }
+        // Update is called once per frame
+    }
+    }
