6a2313f862146474ae116a3935c7572cf6440a9e
leejiwon98 <84241553+leejiwon98@users.noreply.github.com>
2022-06-08 23:00:26 +0900

    mini
    jiwon

---
+++ Assets/jiwon/MinimapUI.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MinimapUI : MonoBehaviour
+{
+    [SerializeField]
+    private Transform left;
+    [SerializeField]
+    private Transform right;
+    [SerializeField]
+    private Transform top;
+    [SerializeField]
+    private Transform bottom;
+
+    [SerializeField]
+    private Transform minimapImage;
+    [SerializeField]
+    private Transform minimapPlayerImage;
+
+    private MOVE targetPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        var inst = Instantiate(minimapImage.material);
+        minimapImage.material = inst;
+
+        targetPlayer = testplayer.MyRoomPlayer.myCharacter;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(targetPlayer != null)
+        {
+            Vector2 mapArea = new Vector2(Vector3.Distance(left.position, right.position), Vector3.Distance(bottom.position, top.position));
+            Vector2 charPos = new Vector2(Vector3.Distance(left.position, new Vector3(targetPlayer.transform.position.x, 0f, 0f)),
+            Vector3.Distance(bottom.position, new Vector3(0f, targetPlayer.transform.position.y, 0f)));
+            Vector2 normalPos = new Vector2(charPos.x / mapArea.x, charPos.y / mapArea.y);
+
+            minimapPlayerImage.rectTransform.anchoredPosition = new Vector2(minimapImage.rectTransform.sizeDelta.x * normalPos.x, minimapImage.rectTransform.sizeDelta.y * normalPos.y);
+        }
+    }
+    public void Open()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public void Close()
+    {
+        gameObject.SetActive(false);
+    }
+}
