db75a0f0db984d95b819fa21ca0fa297f203f05d
minwoo00044 <minwoo0004@gmail.com>
2022-06-10 21:57:39 +0900

    death

--- Assets/utopia/GameManager.cs
+++ Assets/utopia/GameManager.cs
@@ -15,6 +15,7 @@ public class GameManager : MonoBehaviour
     public GameObject menuset;
     public Text questtalk;
     public GameObject Player;
+    public GameObject death_panel;
     public skill_image_manager skill_Image_Manager;
 
 
@@ -35,6 +36,8 @@ public class GameManager : MonoBehaviour
     public Image click_skillimage;
     public Image shift_skillimage;
     public Image q_skillimage;
+
+    public bool death = false;
     void Start()
     {
         click_skillname = 0;
@@ -174,7 +177,16 @@ public class GameManager : MonoBehaviour
 
         skillport(click_skillname, shift_skillname, q_skillname);
 
-
+        if (death)
+        {
+            death_panel.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            death_panel.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
     void skillport(int click_skillindex, int shift_skillinedx, int q_skillindex)
     {
@@ -196,6 +208,7 @@ public class GameManager : MonoBehaviour
     }
     public void GameLoad()
     {
+        death = false;
         if (!PlayerPrefs.HasKey("PlayerX"))
             return;
         float x = PlayerPrefs.GetFloat("PlayerX");
