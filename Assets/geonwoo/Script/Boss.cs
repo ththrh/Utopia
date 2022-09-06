5ad128de6a048f830fccd800160075cb41b72b0c
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-09-07 06:34:44 +0900

    fsm 제작중

--- Assets/geonwoo/Script/Boss.cs
+++ Assets/geonwoo/Script/Boss.cs
@@ -2,17 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum BossState { MoveToAppearPoint = 0, Phase01,}
+
 public class Boss : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    [Serializable]
+    private float bossApperPoint = 2.5f;
+    private BossState bossState = BossState.MoveToAppearPoint;
+    private Movement2D movement2D;
+
+    private void private void Awake() 
     {
-        
+        movement2D = GetComponent<Movement2D>();
+    }
+    
+
+    public void ChangeState(BossState newState)
+    {
+        StopCoroutine(bossState.ToString());
+
+        bossState = new State;
+
+        StartCoroutine(bossState.ToString());
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator MoveToAppearPoint()
     {
-        
+        movement2D.MoveTo(Vector3.down);
+
+        while(true)
+        {
+            if(transform.position.y <= bossApperPoint)
+            {
+                movement2D.MoveTo(Vector3.zero);
+            }
+
+            yeild return null;
+        }
+    }
+
+    private IEnumerator Phase01()
+    {
+        bossWeapon.StartFiring(AttackType.CircleFire);
+
+        while(true)
+        {
+            yield return null;
+        }
     }
 }
---
+++ Assets/geonwoo/Script/BossWeapon.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AttackType {CircleFire = 0,}
+
+public class BossWeapon : MonoBehaviour
+{
+    [Serializable]
+    private GameObject projectilePrefab;
+
+    public void StartFiring(AttackType attackType)
+    {
+        StartCoroutine(attackType.ToString());
+    }
+    // Start is called before the first frame update
+    public void StopFiring(AttackType attackType)
+    {
+        StopCoroutine(attackType.ToString());
+    }
+
+    private IEnumerator CircleFire()
+    {
+        float attackRate = 0.5f;
+        int count = 30;
+        float intervalAngle = 360/count;
+        float weightAngle = 0;
+
+        while(true)
+        {
+            for(int i = 0; i<count;++ i)
+            {
+                GameObject clone = instantiation(projectilePrefab, transform.position, Quaternion.identity);
+                
+                float angle = weightAngle + intervalAngle * i;
+
+                float x = Mathf.Cos(angle * Mathf.PI / 180.0f);
+                float y = Mathf.Sin(angle * Mathf.PI / 180.0f);
+
+                clone.GetComponent<Movement2D>().MoveTo(new Vector2(x,y));
+            
+            }
+
+            wightAngle += 1;
+
+            yield return new WaitForSeconds(attackRate);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
