601e615f31b16c3563021a344df8e69c975c3e18
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-10-06 03:48:45 +0900

    feat : 경로 설정

---
+++ Assets/geonwoo/Script/WayPoint.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPoint : MonoBehaviour
+{
+
+    [SerializeField] Transform[] foodPos;
+    [SerializeField] float speed = 5f;
+    int foodNum = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = foodPos[foodNum].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePath();
+    }
+    public void MovePath()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,foodPos[foodNum].transform.position,speed * Time.deltaTime);
+    
+        if(transform.position == foodPos[foodNum].transform.position)
+        {
+            foodNum++;
+        }
+        if(foodNum == foodPos.Length)
+        {
+            foodNum = 0;
+        }
+        
+
+    }
+
+}
