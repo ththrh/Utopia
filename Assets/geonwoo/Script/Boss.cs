9fbd6177ce989177eaafb0356b9d28ece59cd664
Rose <82176176+geonwoo0215@users.noreply.github.com>
2022-10-09 03:47:40 +0900

    feat : 보스 패턴 수정

--- Assets/geonwoo/Script/Boss.cs
+++ Assets/geonwoo/Script/Boss.cs
@@ -56,7 +56,7 @@ public class Boss : MonoBehaviour
                 gunpoint3.SetActive(true);
             }
 
-            yield return new WaitForSeconds(3.0f);
+            yield return new WaitForSeconds(5.0f);
         }
     }
     
